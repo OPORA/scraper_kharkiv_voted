416b435e0e8feee79e22ac8137e5d7c333038486
Beastie PC <gskondratenko@opora.org.ua>
2017-08-30 16:40:13 +0300

    Change scrape  in directoria files

--- lib/scrape_all_page.rb
+++ lib/scrape_all_page.rb
@@ -1,29 +1,25 @@
 require_relative 'get_page'
 require_relative 'scrape_all_votes'
 require_relative 'get_mps'
-
+require 'tmpdir'
 class GetPages
   def initialize
-    url = "http://www.city.kharkov.ua/uk/gorodskaya-vlast/gorodskoj-sovet/pomenne-golosuvannya.html"
-    page = GetPage.page(url)
-    @all_page = get_all_page(page)
+    #url = "http://www.city.kharkov.ua/uk/gorodskaya-vlast/gorodskoj-sovet/pomenne-golosuvannya.html"
+    #page = GetPage.page(url)
+    @all_page = get_all_page()
     $all_mp = GetMp.new
   end
-  def get_all_page(page)
+  def get_all_page()
+
     hash = []
-    page.css('.container .container_department h3 a').each do |a|
-      if a[:href].include?("http:")
-        hash << { url: a[:href].gsub(/\n/,''), date: a.text }
-      else
-        hash << { url: "http://www.city.kharkov.ua" + a[:href].gsub(/\n/,''), date: a.text }
-      end
-      #p a.text
+    Dir.glob("#{File.dirname(__FILE__)}/../files/*").each do |f|
+       hash << { path: f, date:  f.split('/').last }
     end
     return hash
   end
   def get_all_votes
     @all_page.each do |p|
-      GetAllVotes.votes(p[:url], p[:date])
+      GetAllVotes.votes(p[:path], p[:date])
     end
   end
 end
--- lib/scrape_all_votes.rb
+++ lib/scrape_all_votes.rb
@@ -6,27 +6,9 @@ require_relative 'voted'
 require_relative 'get_mps'
 
 class GetAllVotes
-  def self.votes(url, date)
-     file_path = url
-     p file_path
-     if file_path == "http://www.city.kharkov.ua/documents/uploaded/%D0%9E%D0%9A2%20%D1%81%D0%B5%D1%81%D1%81%D0%B8%D1%8F_%D0%BF%D0%BE%D0%B8%D0%BC%D1%91%D0%BD%D0%BD%D0%BE%D0%B5-%D0%A1%20%D0%9D%D0%90%D0%97%D0%92%D0%90%D0%9D%D0%98%D0%AF%D0%9C%D0%98%20%D0%92%D0%9E%D0%9F%D0%A0%D0%9E%D0%A1%D0%9E%D0%92%20-%20%D0%B4%D0%BB%D1%8F%20%D1%81%D0%BB%D0%B8%D1%8F%D0%BD%D0%B8%D1%8F.rtf"
-       file_name = "#{File.dirname(__FILE__)}/../files/2s.doc"
-     elsif file_path == "http://www.city.kharkov.ua/assets/files/docs/session/4sespoimen.rtf" #or
-       file_name = "#{File.dirname(__FILE__)}/../files/4sespoimen.doc"
-     elsif file_path == "http://www.city.kharkov.ua/assets/files/docs/session/piimen_7-7.rtf"
-       file_name = "#{File.dirname(__FILE__)}/../files/piimen_7-7.doc"
-     else
-       file_name = "#{File.dirname(__FILE__)}/../files/#{Base32.encode(file_path)}"
-     end
+  def self.votes(path, date)
 
-     if (!File.exists?(file_name) || File.zero?(file_name))
-        # uri = URI.encode(file_path.gsub(/%20/,' '))
-        puts ">>>>  File not found, Downloading...."
-        File.write(file_name, open(url).read)
-        p "end download"
-     end
-
-     ReadFile.new.rtf(file_name).each_with_index do |vot, index |
+     ReadFile.new.rtf(path).each_with_index do |vot, index |
          next if vot[:votes].nil?
          number = index + 1
          event = VoteEvent.first(name: vot[:name], date_vote: vot[:date], number: number, date_caden: date, rada_id: 2, option: vot[:result])
